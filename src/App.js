8eff57f09823953e7ef77de63713d0f53012039b
Eunice Baiden <ebaiden@techie.com>
2018-10-07 20:17:04 -0400

    initMap is now visible and rendered

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 
 class App extends Component {
 
-  loadMap = () => {
+  //Render Map 
+  //Life cycle event
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  //**renderMap = loadMap
+
+  renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB7U8rnzGYWVDQjNRYv5Iy-abLyaZcFG40&callback=initMap")
+    window.initMap = this.initMap
   }
 
   initMap = () => {
-        const map = new window.google.maps.Map(document.getElementById('map'), {
+        var map = new window.google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
@@ -37,7 +46,7 @@ class App extends Component {
     */
 
 function loadScript(url) {
-  var index = window.document.getElementByTagName("script")[0]
+  var index = window.document.getElementsByTagName("script")[0]
   var script = window.document.createElement("script")
   script.src = url
   script.async = true
